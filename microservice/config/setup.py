35025045ac1327e821d6ff9cfc3a255e15aa987b
kkf017 <cpunk1017@gmail.com>
2025-08-31 15:37:09 +0200

    bug fix - correct env var

--- microservice/config/setup.py
+++ microservice/config/setup.py
@@ -1,5 +1,5 @@
 import os 
 
-DATABASE = os.getenv('DATABASE', "./files/db_test.db")
-OUTPUT = os.getenv('FILEPATH', "./files/db_test.xlsx")
+DATABASE = os.getenv('DATABASE', "/data/arch-microservice/files/db_test.db")
+FILEPATH = os.getenv('FILEPATH', "/data/arch-microservice/files/db_test.xlsx")
 TABLE = os.getenv('TABLE', "project")
