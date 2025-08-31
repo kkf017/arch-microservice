627e02ff37bb3eca24e806efde54f10ee08f99bd
kkf017 <cpunk1017@gmail.com>
2025-08-31 16:14:02 +0200

    feature - correct run.py

---
+++ microservice/core/run.py
@@ -0,0 +1,19 @@
+import os
+from microservice.config.setup import *
+from microservice.logging_module.handler import logger
+import microservice.infrastructure.sql_driver, microservice.infrastructure.csv_driver, microservice.core.handling_datetime.handling_datetime
+
+
+def run()->None:
+	""" Main function to run service. """
+	if not microservice.core.handling_datetime.handling_datetime.check_datetime_files():
+		logger.info(f"No file created.")
+		return
+	try:
+		columns = microservice.infrastructure.sql_driver.get_columns_name(DATABASE, TABLE)
+		x = microservice.infrastructure.sql_driver.request(DATABASE, f'''SELECT * FROM {TABLE};''')
+		X = microservice.infrastructure.csv_driver.from_db_to_dataframe(x, columns)
+		microservice.infrastructure.csv_driver.writer(FILEPATH, X)
+		logger.info(f"File created successfully.")
+	except Exception as err:
+		logger.error(f"{err}")
